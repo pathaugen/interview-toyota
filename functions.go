7a5aec68a40d422c2c3c9775ad7f1d8330e10313
Patrick William Haugen <pathaugen@gmail.com>
2019-01-30 07:55:18 -0800

    Final Working Copy

--- functions.go
+++ functions.go
@@ -2,7 +2,7 @@
 package main
 
 import (
-  "bytes"
+  //"bytes"
   "encoding/json"
 	"fmt"
   "io/ioutil"
@@ -56,7 +56,7 @@ func webStatus() {
 // Webserver Setup
 func webConfig() {
   http.HandleFunc("/", handlerRoot)
-  http.HandleFunc("/stock", handlerStock)
+  http.HandleFunc("/stock/", handlerStock)
 }
 
 
@@ -103,77 +103,173 @@ func handlerStock(w http.ResponseWriter, r *http.Request) {
   consoleOutput := ``
   webOutput := ``
 
+  // Log each request made to /stock
   webserverRequests += 1
-
   consoleOutput += cBold + cCyan + "Webserver Request" + cClr + " #" + cYellow + strconv.Itoa(webserverRequests) + cClr
-  consoleOutput += breakspace + cBold + cCyan + "Request URL:" + cClr + " http://localhost:" + cClr + cYellow + "PORT" + cClr + "/stock/" + cClr + cYellow + "STOCK" + cClr + "/?stock_exchange=" + cClr + cYellow + "xxx" + cClr
-  consoleOutput += breakspace + cBold + cCyan + "Response:" + cClr + breakspace
+  consoleOutput += breakspace + cBold + cCyan + "Request URL:" + cClr + " http://localhost:" + cClr + cYellow + webserverPort + cClr + "/stock/" + cClr
 
-  // Set the request URL
-  restUrl := "https://www.worldtradingdata.com/api/v1/stock" + "?api_token=" + apiToken + "&symbol=" + "AAPL" + "&stock_exchange=" + "NYSE"
-  response, err := http.Get( restUrl )
-  if err != nil {
-    consoleOutput += "The HTTP request failed with error:" + breakspace + cRed + err.Error() + cClr + breakspace
-  } else {
-    // Example Response:
-    //{
-    //  "symbols_requested": 1,
-    //  "symbols_returned": 1,
-    //  "data": [
-    //    {
-    //      "symbol": "AAPL",
-    //      "name": "Apple Inc.",
-    //      "currency": "USD",
-    //      "price": "154.68",
-    //      "price_open": "156.25",
-    //      "day_high": "158.13",
-    //      "day_low": "154.11",
-    //      "52_week_high": "233.47",
-    //      "52_week_low": "142.00",
-    //      "day_change": "-1.62",
-    //      "change_pct": "-1.04",
-    //      "close_yesterday": "156.30",
-    //      "market_cap": "731605893397",
-    //      "volume": "25718",
-    //      "volume_avg": "42370729",
-    //      "shares": "4729803000",
-    //      "stock_exchange_long": "NASDAQ Stock Exchange",
-    //      "stock_exchange_short": "NASDAQ",
-    //      "timezone": "EST",
-    //      "timezone_name": "America/New_York",
-    //      "gmt_offset": "-18000",
-    //      "last_trade_time": "2019-01-29 16:00:01"
-    //    }
-    //  ]
-    //}
-    // Desired Responses:
-    // symbol
-    // name
-    // price
-    // close_yesterday
-    // currency
-    // market_cap
-    // volume
-    // timezone
-    // timezone_name
-    // gmt_offset
-    // last_trade_time
-    data, _ := ioutil.ReadAll(response.Body)
-    consoleOutput += string(data)
-  }
-  consoleOutput += breakspace + breakspace
+  pageRequestedString := strings.TrimSuffix( strings.TrimPrefix(r.URL.Path, "/"), "/")
+  pageRequestedStringArray := strings.Split( pageRequestedString, "/" )
 
-  jsonData := map[string]string{"firstname": "Nic", "lastname": "Raboy"}
-  jsonValue, _ := json.Marshal(jsonData)
-  response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
-  if err != nil {
-    consoleOutput += "The HTTP request failed with error:" + breakspace + cRed + err.Error() + cClr + breakspace
-  } else {
-    data, _ := ioutil.ReadAll(response.Body)
-    consoleOutput += string(data)
+  if len( pageRequestedStringArray ) == 1 {
+    consoleOutput += breakspace + cRed + "Warning: No Stock Specified!" + cClr + breakspace + breakspace // Show Error
+    webOutput += `{"warning":"You must specify a stock!"}`
+  } else if len( pageRequestedStringArray ) > 1 {
+
+    // Selected Stock
+    selectedStock := pageRequestedStringArray[1]
+    consoleOutput += cYellow + selectedStock + cClr + "/"
+
+    // Selected Exchange
+    selectedExchange := "AMEX" // e.g. AMEX,NASDAQ,NYSE
+    selectedExchangeArr := make(map[string]bool)
+    if r.URL.Query().Get("stock_exchange") != "" {
+      selectedExchange = r.URL.Query().Get("stock_exchange")
+
+      // For each CSV, append to array
+    	selectedExchangeArray := strings.Split( selectedExchange, "," )
+      for i := 0; i < len(selectedExchangeArray); i++ {
+        selectedExchangeArr[selectedExchangeArray[i]] = true
+      }
+
+      consoleOutput += "?stock_exchange=" + cClr + cYellow + selectedExchange + cClr
+    } else {
+      selectedExchangeArr["AMEX"] = true
+    }
+
+    // User Selection Output
+    consoleOutput += breakspace + cBold + cCyan + "Selected Stock: " + cClr + cYellow + selectedStock + cClr
+    if r.URL.Query().Get("stock_exchange") != "" {
+      consoleOutput += breakspace + cBold + cCyan + "Selected Exchange(s): " + cClr + cYellow + selectedExchange + cClr
+    }
+
+    // Set the request URL
+    restUrl := "https://www.worldtradingdata.com/api/v1/stock" + "?api_token=" + apiToken + "&symbol=" + selectedStock
+    if r.URL.Query().Get("stock_exchange") != "" {
+      restUrl += "&stock_exchange=" + selectedExchange
+    }
+
+    // REST API via GET
+    response, err := http.Get( restUrl )
+
+    // REST API via POST
+    //jsonData := map[string]string{ "field1": "value1", "field2": "value2" }
+    //jsonValue, _ := json.Marshal(jsonData)
+    //response, err = http.Post( restUrl, "application/json", bytes.NewBuffer(jsonValue) )
+
+    if err != nil {
+      consoleOutput += "The HTTP request failed with error:" + breakspace + cRed + err.Error() + cClr + breakspace
+    } else {
+
+      // Example Response from REST API:
+      //{
+      //  "symbols_requested": 1,
+      //  "symbols_returned": 1,
+      //  "data": [
+      //    {
+      //      "symbol": "AAPL",
+      //      "name": "Apple Inc.",
+      //      "currency": "USD",
+      //      "price": "154.68",
+      //      "price_open": "156.25",
+      //      "day_high": "158.13",
+      //      "day_low": "154.11",
+      //      "52_week_high": "233.47",
+      //      "52_week_low": "142.00",
+      //      "day_change": "-1.62",
+      //      "change_pct": "-1.04",
+      //      "close_yesterday": "156.30",
+      //      "market_cap": "731605893397",
+      //      "volume": "25718",
+      //      "volume_avg": "42370729",
+      //      "shares": "4729803000",
+      //      "stock_exchange_long": "NASDAQ Stock Exchange",
+      //      "stock_exchange_short": "NASDAQ",
+      //      "timezone": "EST",
+      //      "timezone_name": "America/New_York",
+      //      "gmt_offset": "-18000",
+      //      "last_trade_time": "2019-01-29 16:00:01"
+      //    }
+      //  ]
+      //}
+
+      // Desired Output to End User:
+      // symbol
+      // name
+      // price
+      // close_yesterday
+      // currency
+      // market_cap
+      // volume
+      // timezone
+      // timezone_name
+      // gmt_offset
+      // last_trade_time
+
+      byteValue, _ := ioutil.ReadAll(response.Body)
+      // fmt.Println( string(byteValue) ) // DEBUG
+
+      // Initialize our Stock array
+      var stock Stock
+      json.Unmarshal( []byte(byteValue), &stock )
+      //fmt.Println( stock ) // DEBUG
+
+      if len(stock.StockData) > 0 {
+        consoleOutput += breakspace + cBold + cCyan + "Response:" + cClr + breakspace
+        for i := 0; i < len(stock.StockData); i++ {
+
+          //consoleOutput += "symbols_requested: " + strconv.Itoa(stock.SymbolsRequested) + breakspace // DEBUG
+          //consoleOutput += "symbols_returned: " + strconv.Itoa(stock.SymbolsRequested) + breakspace // DEBUG
+
+          consoleOutput += "stock_exchange_short: " + cYellow + stock.StockData[i].StockExchangeShort + cClr + breakspace
+
+          consoleOutput += "symbol: " + cYellow + stock.StockData[i].Symbol + cClr + breakspace
+          consoleOutput += "name: " + cYellow + stock.StockData[i].Name + cClr + breakspace
+          consoleOutput += "price: " + cYellow + stock.StockData[i].Price + cClr + breakspace
+          consoleOutput += "close_yesterday: " + cYellow + stock.StockData[i].CloseYesterday + cClr + breakspace
+          consoleOutput += "currency: " + cYellow + stock.StockData[i].Currency + cClr + breakspace
+          consoleOutput += "market_cap: " + cYellow + stock.StockData[i].MarketCap + cClr + breakspace
+          consoleOutput += "volume: " + cYellow + stock.StockData[i].Volume + cClr + breakspace
+          consoleOutput += "timezone: " + cYellow + stock.StockData[i].Timezone + cClr + breakspace
+          consoleOutput += "timezone_name: " + cYellow + stock.StockData[i].TimezoneName + cClr + breakspace
+          consoleOutput += "gmt_offset: " + cYellow + stock.StockData[i].GmtOffset + cClr + breakspace
+          consoleOutput += "last_trade_time: " + cYellow + stock.StockData[i].LastTradeTime + cClr + breakspace
+
+          // Skip any data where stock_exchange_short isn't part of user's requested selectedExchange
+          if selectedExchangeArr[stock.StockData[i].StockExchangeShort] {
+
+            webOutput += `{"` + stock.StockData[i].StockExchangeShort + `":{
+              "symbol":"` + stock.StockData[i].Symbol + `",
+              "name":"` + stock.StockData[i].Name + `",
+              "price":"` + stock.StockData[i].Price + `",
+              "close_yesterday":"` + stock.StockData[i].CloseYesterday + `",
+              "currency":"` + stock.StockData[i].Currency + `",
+              "market_cap":"` + stock.StockData[i].MarketCap + `",
+              "volume":"` + stock.StockData[i].Volume + `",
+              "timezone":"` + stock.StockData[i].Timezone + `",
+              "timezone_name":"` + stock.StockData[i].TimezoneName + `",
+              "gmt_offset":"` + stock.StockData[i].GmtOffset + `",
+              "last_trade_time":"` + stock.StockData[i].LastTradeTime + `"
+              }}`
+
+          } else {
+            consoleOutput += cRed + "Warning: stock_exchange_short doesn't match user's exchange selection parameters!" + cClr + breakspace
+            webOutput += `{ }`
+          }
+
+        }
+      } else {
+        consoleOutput += breakspace + cRed + string(byteValue) + cClr // Show Error
+        webOutput += string(byteValue)
+      }
+
+    }
+    consoleOutput += breakspace + breakspace
   }
-  consoleOutput += breakspace + breakspace
 
   if debug { fmt.Print( consoleOutput ) }
-	fmt.Fprintf( w, webOutput )
+
+  w.Header().Set("Content-Type", "application/json; charset=utf-8")
+  //fmt.Fprint(w, "{" + webOutput + "}")
+  fmt.Fprint(w, webOutput )
 }
--- structs.go
+++ structs.go
@@ -7,15 +7,32 @@ import (
 
 // Stock Struct
 type Stock struct {
-  symbol            string
-  name              string
-  price             string
-  close_yesterday   string
-  currency          string
-  market_cap        string
-  volume            string
-  timezone          string
-  timezone_name     string
-  gmt_offset        string
-  last_trade_time   string
+  SymbolsRequested  int         `json:"symbols_requested"`
+  SymbolsReturned   int         `json:"symbols_returned"`
+  StockData         []StockData `json:"data"`
+}
+
+type StockData struct {
+  Symbol                string  `json:"symbol"`           // Return Value
+  Name                  string  `json:"name"`             // Return Value
+  Currency              string  `json:"currency"`         // Return Value
+  Price                 string  `json:"price"`            // Return Value
+  PriceOpen             string  `json:"price_open"`
+  DayHigh               string  `json:"day_high"`
+  DayLow                string  `json:"day_low"`
+  WeekHigh              string  `json:"52_week_high"`
+  WeekLow               string  `json:"52_week_low"`
+  DayChange             string  `json:"day_change"`
+  ChangePct             string  `json:"change_pct"`
+  CloseYesterday        string  `json:"close_yesterday"`  // Return Value
+  MarketCap             string  `json:"market_cap"`       // Return Value
+  Volume                string  `json:"volume"`           // Return Value
+  VolumeAvg             string  `json:"volume_avg"`
+  Shares                string  `json:"shares"`
+  StockExchangeLong     string  `json:"stock_exchange_long"`
+  StockExchangeShort    string  `json:"stock_exchange_short"`
+  Timezone              string  `json:"timezone"`         // Return Value
+  TimezoneName          string  `json:"timezone_name"`    // Return Value
+  GmtOffset             string  `json:"gmt_offset"`       // Return Value
+  LastTradeTime         string  `json:"last_trade_time"`  // Return Value
 }
